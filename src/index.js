712e0b8f9a86d5654583ea6dc5859c69621d7f69
Arttu Laitinen <124583634+Arttu73@users.noreply.github.com>
2023-07-29 09:18:02 +0000

    Pull

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import "./styles.css";
+
+let game;
+
+const gameOptions = {
+  playerGravity: 1000,
+  playerSpeed: 300,
+};
+
+window.onload = function () {
+  let gameConfig = {
+    type: Phaser.AUTO,
+    backgroundColor: "#003300",
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      width: 800,
+      Height: 1000,
+    },
+    pixelArt: true,
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: {
+          y: 0,
+        },
+      },
+    },
+    scene: PlayGame,
+  };
+
+  game = new Phaser.Game(gameConfig);
+  window.focus();
+};
+
+class PlayGame extends Phaser.Scene {}
